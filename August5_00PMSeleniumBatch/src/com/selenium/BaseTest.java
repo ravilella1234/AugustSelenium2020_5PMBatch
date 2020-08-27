9b7da0a9d70f61502ba1006a3aff75881ed18d5e
maha <maha@gmail.com>
2020-08-27 18:01:01 +0530

    loding instance urls dynamically & Browser commands

--- August5_00PMSeleniumBatch/src/com/selenium/BaseTest.java
+++ August5_00PMSeleniumBatch/src/com/selenium/BaseTest.java
@@ -14,12 +14,25 @@ public class BaseTest
 	public static FileInputStream fis;
 	public static String projectpath = System.getProperty("user.dir");
 	public static Properties dp;
+	public static Properties parentProp;
+	public static Properties childProp;
 	
 	public static void init() throws Exception
 	{
 		fis = new FileInputStream(projectpath +"//data.properties");
 		dp = new Properties();
 		dp.load(fis);
+		
+		fis = new FileInputStream(projectpath +"//environment.properties");
+		parentProp = new Properties();
+		parentProp.load(fis);
+		String e = parentProp.getProperty("env");
+		System.out.println(e);
+		
+		fis = new FileInputStream(projectpath + "//" + e + ".properties");
+		childProp = new Properties();
+		childProp.load(fis);
+		System.out.println(childProp.getProperty("amazonurl"));
 	}
 	
 	public static void launch(String browser)
@@ -38,7 +51,7 @@ public class BaseTest
 	
 	public static void navigateUrl(String url)
 	{
-		driver.get(dp.getProperty(url));
+		driver.get(childProp.getProperty(url));
 	}
 	
 
