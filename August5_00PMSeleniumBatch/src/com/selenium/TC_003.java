947e8518b3f187b706dde92165af71fb88251a9f
maha <maha@gmail.com>
2020-09-14 18:01:38 +0530

    Locators Indroduction

--- August5_00PMSeleniumBatch/src/com/selenium/TC_003.java
+++ August5_00PMSeleniumBatch/src/com/selenium/TC_003.java
@@ -1,5 +1,10 @@
 package com.selenium;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 public class TC_003 extends BaseTest
 {
 
@@ -9,7 +14,29 @@ public class TC_003 extends BaseTest
 		
 		launch("chromebrowser");
 
-		navigateUrl("iciciurl");
+		navigateUrl("amazonurl");
+		
+		driver.findElement(By.id("searchDropdownBox")).sendKeys("Books");
+		
+		WebElement loc = driver.findElement(By.name("field-keywords"));
+		loc.sendKeys("sony");
+		Thread.sleep(3000);
+		loc.clear();
+		Thread.sleep(3000);
+		loc.sendKeys("Harry Potter");
+		
+		
+		driver.findElement(By.xpath("//div[@class='nav-search-submit nav-sprite']//input[@class='nav-input']")).click();
+		
+		//driver.findElement(By.linkText("Computers")).click();
+		
+		//driver.findElement(By.partialLinkText("Computer")).click();
+		
+		List<WebElement> links = driver.findElements(By.tagName("a"));
+		for(int i=0;i<links.size();i++)
+		{
+			System.out.println(links.get(i).getText());
+		}
 	}
 
 }
