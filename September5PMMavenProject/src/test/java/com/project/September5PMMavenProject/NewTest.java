2aa36168582c71caed119afeaab787901a2fc974
maha <maha@gmail.com>
2020-09-25 17:52:45 +0530

    TestNG Day-1

---
+++ September5PMMavenProject/src/test/java/com/project/September5PMMavenProject/NewTest.java
@@ -0,0 +1,13 @@
+package com.project.September5PMMavenProject;
+
+import org.testng.annotations.Test;
+
+public class NewTest 
+{
+	
+  @Test
+  public void f() 
+  {
+	  System.out.println("iam f test");
+  }
+}
